ff1d2d04939387d50f32da5ab7a9f6b9e5c9b1bd
Drew Keefer <38295463+DrewKeefer@users.noreply.github.com>
2023-10-02 20:58:25 -0400

    progress

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+const axios = require('axios');
+const cheerio = require('cheerio');
+const fs = require('fs');
 
 function addClickEventsToGridItems() {
-    let allQuestions = document.getElementsByClassName("question");
+    let allQuestions = document.getElementsByClassName('question');
     for (let i = 0; i < allQuestions.length; i++) {
         allQuestions[i].onclick = (e) => {
             displayQuestion(i);
@@ -10,5 +13,26 @@ function addClickEventsToGridItems() {
 addClickEventsToGridItems();
 
 function displayQuestion(index) {
-    console.log(`Question ${index} was clicked`)
+    console.log(`Question ${index} was clicked`);
 };
+
+function loadGame(){
+    console.log('Game is loading');
+
+
+
+    document.getElementsByClassName('splash')[0].style.display ='none';
+}
+
+
+async function mainLoad(){
+    try{
+        const { data } = await axios.get('https://j-archive.com/');
+        const $ = cheerio.load('')
+        console.log($);
+    } catch (error) {
+        console.log('oh no');
+    };
+};
+
+mainLoad();
